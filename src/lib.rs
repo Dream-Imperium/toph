79426992c234a82c20f1db10006e88cb9afb453a
Iwakura <iwakuramarie@waifu.club>
2021-01-07 11:27:20 -0500

    Change version to v1.0.0

--- src/lib.rs
+++ src/lib.rs
@@ -63,7 +63,7 @@ use std::path::{Path, PathBuf};
 use std::time::{Duration, Instant};
 
 /// Program version.
-pub const VERSION: &str = "0.4.0";
+pub const VERSION: &str = "1.0.0";
 
 #[global_allocator]
 pub static ALLOCATOR: alloc::Allocator = alloc::Allocator::new(System);
